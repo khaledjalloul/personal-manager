dcb61e0da3c2d9e6c468d0511b9096bc7ef72517
Khaled Jalloul <khaled.jalloul@hotmail.com>
2026-04-27 10:08:37 +0000

    Move docker files to separate folder

--- .devcontainer/post_create.bash
+++ .devcontainer/post_create.bash
@@ -1,11 +1,13 @@
+# Install dependencies for the backend and run database migrations
 cd /workspace/personal-manager/backend_expressjs
 npm install
 npx prisma migrate dev
 
+# Install dependencies for the frontend
 cd /workspace/personal-manager/frontend_reactjs
 npm install
 
+# Add aliases to start the frontend and backend easily
 echo "alias start-frontend='npm --prefix /workspace/personal-manager/frontend_reactjs start'" >> ~/.bashrc
 echo "alias start-backend='npm --prefix /workspace/personal-manager/backend_expressjs start'" >> ~/.bashrc
 echo 'alias start-all="npm --prefix /workspace/personal-manager/frontend_reactjs start & npm --prefix /workspace/personal-manager/backend_expressjs start"' >> ~/.bashrc
-source ~/.bashrc
